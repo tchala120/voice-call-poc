f1c23ffecb91b83ad2ea29a43583e251c1d1d070
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-06-23 18:16:49 +0700

    feat: Added room page

---
+++ src/pages/RoomPage.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react'
+
+import Center from 'components/Center'
+
+const RoomPage: FC = () => {
+  return (
+    <Center>
+      <h1>Room Page</h1>
+    </Center>
+  )
+}
+
+export default RoomPage
