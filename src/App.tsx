5c822290d843ced688296d25d9f5df7dced135ea
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-06-23 18:15:57 +0700

    feat: Wrapped with auth provider and setup the local storage

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,18 @@ import type { FC } from 'react'
 
 import PageRouter from 'setup/PageRouter'
 
+import setupLocalStorage from 'services/localStorage'
+
+import { AuthProvider } from 'context/useAuthContext'
+
+setupLocalStorage()
+
 const App: FC = () => {
-  return <PageRouter />
+  return (
+    <AuthProvider>
+      <PageRouter />
+    </AuthProvider>
+  )
 }
 
 export default App
