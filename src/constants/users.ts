807b1ebba1ca0cc9b3ad1b592f555a66961cc8d4
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-06-23 17:51:48 +0700

    feat: Added mock users data

---
+++ src/constants/users.ts
@@ -0,0 +1,29 @@
+interface UserMetadata {
+  username: string
+  password: string
+}
+
+const users: UserMetadata[] = [
+  {
+    username: 'ioniclac',
+    password: '1234567890',
+  },
+  {
+    username: 'eaderymo',
+    password: '1234567890',
+  },
+  {
+    username: 'advehrib',
+    password: '1234567890',
+  },
+  {
+    username: 'dionsest',
+    password: '1234567890',
+  },
+  {
+    username: 'eigalgen',
+    password: '1234567890',
+  },
+]
+
+export default users
