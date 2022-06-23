d1368ab29157591646546f8cc8b3556f3a65990a
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-06-23 17:50:40 +0700

    feat: Added  center component

---
+++ src/components/Center.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+const Center = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+export default Center
