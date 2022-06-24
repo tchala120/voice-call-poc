a493be2ab7c5f0300fe8016c5edca7d07df0c49e
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-06-24 15:31:49 +0700

    feat: Added page layout

---
+++ src/layouts/PageLayout.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled'
+import { Layout } from 'antd'
+
+import AuthenticatedHeader from 'components/AuthenticatedHeader'
+import MainMenu from 'components/MainMenu'
+
+import type { FCWithChildren } from 'types'
+
+const { Sider, Content } = Layout
+
+const PageLayout: FCWithChildren = ({ children }) => {
+  return (
+    <PageLayoutContainer>
+      <Sider>
+        <MainMenu />
+      </Sider>
+      <Layout>
+        <AuthenticatedHeader />
+
+        <Content>{children}</Content>
+      </Layout>
+    </PageLayoutContainer>
+  )
+}
+
+export default PageLayout
+
+const PageLayoutContainer = styled(Layout)`
+  .ant-layout-content {
+    padding: 50px;
+    min-height: calc(100vh - 64px);
+  }
+`
