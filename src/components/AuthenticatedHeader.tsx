5182e945ae6ccfd7217e6c07c887af40dac3e53a
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-06-24 17:06:18 +0700

    feat: Added simple header

---
+++ src/components/AuthenticatedHeader.tsx
@@ -0,0 +1,52 @@
+import type { FC } from 'react'
+
+import styled from '@emotion/styled'
+import { Dropdown, Layout, Menu, Space } from 'antd'
+
+import useAuthContext from 'context/useAuthContext'
+
+const AuthenticatedHeader: FC = () => {
+  const auth = useAuthContext()
+  const authUser = auth.user
+
+  const menu = (
+    <Menu
+      items={[
+        {
+          key: '1',
+          label: <span onClick={auth.signOut}>Log out</span>,
+        },
+      ]}
+    />
+  )
+
+  return (
+    <AuthenticatedHeaderContainer>
+      <span></span>
+
+      <Space>
+        Logged in as
+        <Dropdown overlay={menu} placement="bottomRight" trigger={['click']}>
+          <strong
+            style={{
+              cursor: 'pointer',
+              textDecoration: 'underline',
+              color: '#1890ff',
+            }}
+          >
+            {authUser?.username}
+          </strong>
+        </Dropdown>
+      </Space>
+    </AuthenticatedHeaderContainer>
+  )
+}
+
+export default AuthenticatedHeader
+
+const AuthenticatedHeaderContainer = styled(Layout.Header)`
+  background: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
