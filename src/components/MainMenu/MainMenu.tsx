96ca3a6dc4d9f13568730789da0d23f5941242d6
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-06-25 18:13:13 +0700

    feat: Added logo section

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,9 +1,31 @@
 import type { FC } from 'react'
 
 import Logo from 'components/Logo'
+import styled from '@emotion/styled'
+
+import littleBeanLogo from 'assets/images/little_bean_icon.png'
 
 const MainMenu: FC = () => {
-  return <Logo />
+  return (
+    <>
+      <Logo />
+
+      <LittleBeanLogoContainer>
+        <img src={littleBeanLogo} alt="Little Bean Soft" />
+      </LittleBeanLogoContainer>
+    </>
+  )
 }
 
 export default MainMenu
+
+const LittleBeanLogoContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+
+  > img {
+    width: 100%;
+    height: auto;
+  }
+`
