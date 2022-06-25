77d80d80f5562b15d61118950dd6266b21b1c0d0
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-06-25 17:22:38 +0700

    fix: Changed structure of users

--- src/constants/users.ts
+++ src/constants/users.ts
@@ -1,12 +1,7 @@
-interface VoiceCallRoom {
-  channel: string
-  token: string
-}
-
 export interface UserMetadata {
   userID: number
   username: string
-  room: VoiceCallRoom
+  room: string
   password: string
 }
 
@@ -14,61 +9,37 @@ const users: UserMetadata[] = [
   {
     userID: 1,
     username: 'ioniclac',
-    room: {
-      channel: 'room_a',
-      token:
-        '0066bcb6676562e42b6a4f5ed71591a83c5IADm7PWe964yEnSxWECX7BVZwdphp4hFp8fTl4aLmzDjU4ZClsgAAAAAEABGROOeYQa4YgEAAQBgBrhi',
-    },
+    room: 'room_a',
     password: '1234567890',
   },
   {
     userID: 2,
     username: 'eaderymo',
-    room: {
-      channel: 'room_b',
-      token:
-        '0066bcb6676562e42b6a4f5ed71591a83c5IAAZIhN2WGMBhfBpC/0uNK5bzbR3KbXJR9htgYqvcnrcLjwTn1EAAAAAEABGROOe3J22YgEAAQDcnbZi',
-    },
+    room: 'room_b',
     password: '1234567890',
   },
   {
     userID: 3,
     username: 'advehrib',
-    room: {
-      channel: 'room_c',
-      token:
-        '0066bcb6676562e42b6a4f5ed71591a83c5IADM79F6aqAiCAUAsug2DHZuWfNJowoVl0w+y2/qod2YgKojmCYAAAAAEABGROOehKe2YgEAAQCEp7Zi',
-    },
+    room: 'room_c',
     password: '1234567890',
   },
   {
     userID: 4,
     username: 'dionsest',
-    room: {
-      channel: 'room_d',
-      token:
-        '0066bcb6676562e42b6a4f5ed71591a83c5IADZAlO2Lhn0r7IRfq+IrMhDzA4bMspfGSfC0o73TZP/9gm2/LgAAAAAEABGROOel6e2YgEAAQCXp7Zi',
-    },
+    room: 'room_d',
     password: '1234567890',
   },
   {
     userID: 5,
     username: 'eigalgen',
-    room: {
-      channel: 'room_e',
-      token:
-        '0066bcb6676562e42b6a4f5ed71591a83c5IABevMbHvyLJ2Aav/l2FHhOMWgN7VFyClFjSHyaMOH56U5+G+88AAAAAEABGROOep6e2YgEAAQCnp7Zi',
-    },
+    room: 'room_e',
     password: '1234567890',
   },
   {
     userID: 6,
     username: 'thetoon',
-    room: {
-      channel: 'room_a',
-      token:
-        '0066bcb6676562e42b6a4f5ed71591a83c5IADm7PWe964yEnSxWECX7BVZwdphp4hFp8fTl4aLmzDjU4ZClsgAAAAAEABGROOeYQa4YgEAAQBgBrhi',
-    },
+    room: 'room_a',
     password: '1234567890',
   },
 ]
