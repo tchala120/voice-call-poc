89683a8244de1a06cb2253c93b42a50e194164f0
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-06-25 17:21:15 +0700

    feat: Wrapped with react query client provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import type { FC } from 'react'
 
 import { ConfigProvider } from 'antd'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 import PageRouter from 'setup/PageRouter'
 
@@ -10,13 +12,21 @@ import { AuthProvider } from 'context/useAuthContext'
 
 setupLocalStorage()
 
+const queryClient = new QueryClient()
+
 const App: FC = () => {
   return (
-    <ConfigProvider componentSize="large">
-      <AuthProvider>
-        <PageRouter />
-      </AuthProvider>
-    </ConfigProvider>
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider componentSize="large">
+        <AuthProvider>
+          <PageRouter />
+        </AuthProvider>
+      </ConfigProvider>
+
+      {process.env.NODE_ENV === 'development' && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </QueryClientProvider>
   )
 }
 
